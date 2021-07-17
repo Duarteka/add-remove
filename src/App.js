621b0fb299d8bb93e2a47c6e04d2d91c473b07f9
Duarteka <duarte.karen21@gmail.com>
2021-07-17 14:02:30 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React,{ Component } from 'react';
+import Formulario from './Formulario';
+import Cuadro from './Cuadro';
+
+class Cuadros extends Component{
+  constructor(){
+    super();
+      this.state = {
+        cuadro : []
+      }
+    }
+    crearCuadro= (ancho,alto)=>{
+     var id = this.state.cuadro.length > 0 ? Math.max.apply(Math,this.state.cuadro.map(item => item.id)) + 1 : 1;
+     var cuadros = this.state.cuadro;
+     cuadros.push({
+       id : id,
+       ancho : ancho,
+       alto : alto
+     });
+     this.setState({ cuadros : cuadros });
+    }
+    eliminarCuadro = id => {
+      if(id === this.item.id){
+        return  this.props.eliminarCuadro(this.props.id);
+      }
+      console.log(id)
+    }
+  render(){
+    return(
+      <>
+        <Formulario crearCuadro={this.crearCuadro}/>
+        <div className='contenedor'>
+         { this.state.cuadro.map(item =>  <Cuadro  key={item.id} id={item.id} ancho={item.alto} alto={item.alto} eliminarCuadro={this.eliminarCuadro} />)}
+        </div>
+      </>
+      );
+    }
+} 
 
 
-function App() {
-  return (
-    <h1>Hola</h1>
-  );
-}
+export default Cuadros;
 
-export default App;
